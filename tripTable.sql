56c146caa497503567c18bea69d0e6142e89e1dc
andmos <andreas.mosti@gmail.com>
2018-03-08 14:20:02 +0100

    changed to DateTime datatype

--- tripTable.sql
+++ tripTable.sql
@@ -1,6 +1,6 @@
 CREATE TABLE trip (
     `start_station_id` INT,
-    `start_time` VARCHAR(25) CHARACTER SET utf8,
+    `start_time` DATETIME,
     `end_station_id` INT,
-    `end_time` VARCHAR(25) CHARACTER SET utf8
+    `end_time` DATETIME
 );
